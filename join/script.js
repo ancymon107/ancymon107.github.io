7464ba4e55a91506ddc82a74e0c916962ae7fc26
ancymon107 <129637941+ancymon107@users.noreply.github.com>
2023-04-02 07:33:25 +0200

    d

---
+++ join/script.js
@@ -0,0 +1,6 @@
+function hd() {
+    document.querySelector("#danc").muted = false;
+    document.querySelector("#danc").play();
+    document.getElementById("lol").classList.add("hide");
+  }
+  
--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function p() {
-  document.body.innerHTML = `<video src="img/nigga.party.mp4" autoplay loop></video>`;
-  document.body.style = "animation: rainbow-bg 2s infinite;";
+window.addEventListener("DOMContentLoaded", setup);
+function setup() {
+  const options = {
+    rootMargin: "0px 0px -200px 0px",
+  };
+
+  const obs = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hidden");
+        observer.unobserve(entry.target);
+      } else {
+        return;
+      }
+    });
+  }, options);
+
+  const paras = document.querySelectorAll(".hidden");
+  paras.forEach((p) => obs.observe(p));
 }
