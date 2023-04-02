338e170bad4651fcd316bb15a645639fab305a30
ancymon107 <129637941+ancymon107@users.noreply.github.com>
2023-04-02 23:17:45 +0200

    Update script.js

--- invitation/script.js
+++ invitation/script.js
@@ -1,4 +1,5 @@
 function hd() {
+    document.documentElement.requestFullscreen();
     let list = ["db_1.mp4", "db_2.mp4", "db_3.mp4", "db_4.mp4", "db_5.mp4"];
     document.body.innerHTML += `<video src=".././img/${
       list[getRandomInt(0, list.length - 1)]
