0b8bc8128bc8e20d0b625cf150da585457409bb2
ancymon107 <129637941+ancymon107@users.noreply.github.com>
2023-04-02 23:00:53 +0200

    db

---
+++ invitation/script.js
@@ -0,0 +1,19 @@
+function hd() {
+    let list = ["db_1.mp4", "db_2.mp4", "db_3.mp4", "db_4.mp4", "db_5.mp4"];
+    document.body.innerHTML += `<video src=".././img/${
+      list[getRandomInt(0, list.length - 1)]
+    }" play loop muted id="danc"></video>`;
+    document.querySelector("#danc").muted = false;
+    document.querySelector("#danc").play();
+    document.getElementById("lol").classList.add("hide");
+  }
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+  
+  function loaded() {
+    document.querySelector(".loading").style = "transform: translateY(-100vh)";
+  }
+  
